65fdc25d8cd31d2b8ca996e902d4007e93da4cd0
jihun <gn753@naver.com>
2024-08-30 13:38:49 +0900

    feat: 탭 네비게이션 기능 추가

--- screens/MainTabs.tsx
+++ screens/MainTabs.tsx
@@ -3,15 +3,45 @@ import React from 'react';
 import FeedScreen from './FeedsScreen';
 import CalendarScreen from './CalendarScreen';
 import SearchScreen from './SearchScreen';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Tab = createBottomTabNavigator();
 
 function MainTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Feeds" component={FeedScreen} />
-      <Tab.Screen name="Calendar" component={CalendarScreen} />
-      <Tab.Screen name="Search" component={SearchScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#009688',
+      }}>
+      <Tab.Screen
+        name="Feeds"
+        component={FeedScreen}
+        options={{
+          title: '피드',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="view-stream" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Calendar"
+        component={CalendarScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="event" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="search" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
